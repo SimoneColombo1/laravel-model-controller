7d8742892e2730ac7903e92bfdfbb93467691937
SimoneColombo1 <simcolombo2@gmail.com>
2024-07-17 19:20:01 +0200

    Stampate le cards con i film

--- resources/views/guest/movies/index.blade.php
+++ resources/views/guest/movies/index.blade.php
@@ -5,9 +5,18 @@
 @section('main-content')
     <h1>
        Movies:
-       @foreach ($movies as $movie )
-           {{$movie->title;}}
+     </h1>
+     <section class="movies">
+      @foreach ($movies as $movie )
+      <div>
+      <span> {{$movie->title;}}</span>
+          <span> {{$movie->original_title;}}</span>
+          <span> {{$movie->nationality;}}</span>
+          <span> {{$movie->date;}}</span>
+          <span> {{$movie->vote;}}</span>
+      </div>
+
        @endforeach
-    </h1>
+</section>
 
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,2 +1,4 @@
 <header>
+    <a href="/movies">MOVIES</a>
+    <a href="/">HOME</a>
 </header>
